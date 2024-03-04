70cdd9cc3fb92651599c0a1e7941d0bf06e79ea5
j4nov <142175258+j4nov@users.noreply.github.com>
2024-03-04 20:33:55 +0200

    ...

--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
 import "../css/Registration.css";
 
 function Registration() {
